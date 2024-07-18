ebf803e9116b1d3941f3510e563d8df70676c752
Max Ognenoi <maksymohnenyi>
2024-07-17 23:07:40 -0400

    update13

--- src/components/NavListItem.jsx
+++ src/components/NavListItem.jsx
@@ -4,7 +4,8 @@ function NavListItem({ item }) {
   return (
     <li>
       <a href="#">
-        <i className={`bi ${item.icon}`}></i> {item.name}
+        <i className={`bi ${item.icon}`}></i>
+        <span className="navName">{item.name}</span>
       </a>
     </li>
   );
