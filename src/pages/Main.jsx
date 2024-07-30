e09cd50a0804f64f5dc49a6c845b1a74862dea15
Max Ognenoi <maksymohnenyi>
2024-07-29 22:29:22 -0400

    update31

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./main.css";
 import SideMenu from "../components/SideMenu";
 import Header from "./Header";
+import Home from "./Home";
 
 function Main() {
   const [active, setActive] = useState(false);
@@ -14,6 +15,9 @@ function Main() {
       <SideMenu active={active} />
       <div className={`banner ${active ? "active" : undefined}`}>
         <Header toggleActive={handleToggleActive} />
+        <div className="container-fluid">
+          <Home />
+        </div>
       </div>
     </main>
   );
